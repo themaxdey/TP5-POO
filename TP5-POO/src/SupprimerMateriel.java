78b20705f6c764651c88d1295c556a5bfc5e8c93
Jsbeaulne <jeansebastien321@gmail.com>
2018-05-09 18:12:46 -0400

    SupprimerMateriel

--- TP5-POO/src/SupprimerMateriel.java
+++ TP5-POO/src/SupprimerMateriel.java
@@ -1,5 +1,5 @@
 /**
- * Auteurs : Maxime Dery & Jean-Sebastien Beaulne et Christian Mongeon
+ * Auteurs : Jean-Sébastien Beaulne et Maxime Déry et Christian Mongeon
  * Fichier : SupprimerMateriel.java
  * Cours   : 420-ZE4-MO (TP5, MomoTech)
  * Date    : 15 mai 2018
@@ -9,15 +9,17 @@
 import outilsjava.*;
 
 /**
- * La classe SupprimerMateriel permet de faire des suppressions de matériel informatique, pour le programme de réemploi
- * de matériel informatique de l'entreprise MomoTech. Cette classe implémente l'interface OutilsConstantes.
+ * La classe SupprimerMateriel permet de faire des suppressions de matériel
+ * informatique, pour le programme de réemploi de matériel informatique de
+ * l'entreprise MomoTech. Cette classe implémente l'interface OutilsConstantes.
  */
 
 public class SupprimerMateriel implements OutilsConstantes {
 
 	/**
-	 * Le constructeur SupprimerMateriel() permet de gérer les suppressions de matériel informatique, pour le
-	 * programme de réemploi de matériel informatique de l'entreprise MomoTech. On ne peut pas supprimer un 
+	 * Le constructeur SupprimerMateriel() permet de gérer les suppressions de
+	 * matériel informatique, pour le programme de réemploi de matériel
+	 * informatique de l'entreprise MomoTech. On ne peut pas supprimer un
 	 * matériel informatique qui est assigné à un participant.
 	 * 
 	 * @param momoTech
@@ -26,8 +28,8 @@ public class SupprimerMateriel implements OutilsConstantes {
 
 	public SupprimerMateriel( MomoTech momoTech ) {
 		// Constantes locales.
-		final String MESS_VIDE = "\nImpossible de supprimer du matériel informatique. " +
-								 "Il n'y a aucun matériel destiné au réemploi de disponible.";
+		final String MESS_VIDE = "\nImpossible de supprimer du matériel informatique. "
+				+ "Il n'y a aucun matériel destiné au réemploi de disponible.";
 		final String TITRE = "\nSuppression de matériel informatique.";
 		final String QUEST_CONF_SUPP = "\nVoulez-vous supprimer ce matériel informatique (O ou N) ? ";
 		final String MESS_DEVENU_VIDE = "\nSuppression terminée. Il ne reste plus de matériel informatique.";
@@ -50,12 +52,58 @@ public class SupprimerMateriel implements OutilsConstantes {
 				 * TODO (À COMPLÉTER). Voir page 13 de l'énoncé du TP5.
 				 */
 
+				materiel = MaterielInfo.creerMaterielInfo();
+				materiel.lireNoMateriel();
 
-				//--------------------------------------------------------------------
-				// Reste-t-il du matériel informatique pour le programme de réemploi ?
+				indMat = momoTech.getTabMateriel().chercher( materiel );
+
+				if ( indMat == -1 ) {
+
+					System.out.println(
+							"\nLe matériel informatique numéro " + materiel.getNoMateriel() + " n'existe pas." );
+
+				} else {
+
+					assignation = new Assignation( materiel.getNoMateriel() );
+					indAssign = momoTech.getTabAssignations().chercher( assignation );
+
+					if ( indAssign != -1 ) {
+
+						System.out.println( "\nLa suppression est impossible, puisque le matériel numéro "
+								+ materiel.getNoMateriel() + " est assigné à un participant." );
+
+					} else {
+						char confirmation;
+
+						materiel = (MaterielInfo) momoTech.getTabMateriel().obtenirObjet( indMat );
+
+						materiel.afficher();
+
+						confirmation = OutilsLecture.lireOuiNon( QUEST_CONF_SUPP );
+
+						if ( confirmation == OUI ) {
+
+							momoTech.getTabMateriel().supprimer( indMat );
+							System.out.println( "\nLe matériel informatique numéro " + materiel.getNoMateriel()
+									+ " a été supprimé." );
+
+						} else {
+
+							System.out.println( "\nLa suppression du matériel informatique numéro "
+									+ materiel.getNoMateriel() + " a été annulée." );
+
+						}
+
+					}
+
+				}
+
+				// --------------------------------------------------------------------
+				// Reste-t-il du matériel informatique pour le programme de
+				// réemploi ?
 
 				if ( momoTech.getTabMateriel().estVide() ) {
-					
+
 					System.out.println( MESS_DEVENU_VIDE );
 					rep = NON;
 				} else {
