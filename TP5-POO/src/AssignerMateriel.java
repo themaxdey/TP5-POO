26442df90a88018a82f487590c9886c290a7f748
Jsbeaulne <jeansebastien321@gmail.com>
2018-05-09 18:42:14 -0400

    AssignerMateriel

--- TP5-POO/src/AssignerMateriel.java
+++ TP5-POO/src/AssignerMateriel.java
@@ -1,5 +1,5 @@
 /**
- * Auteurs : Maxime Dery & Jean-Sebastien Beaulne et Christian Mongeon
+ * Auteurs : Jean-Sébastien Beaulne et Maxime Déry et Christian Mongeon
  * Fichier : AssignerMateriel.java
  * Cours   : 420-ZE4-MO (TP5, MomoTech)
  * Date    : 15 mai 2018
@@ -9,16 +9,18 @@
 import outilsjava.*;
 
 /**
- * La classe AssignerMateriel permet d'assigner du matériel informatique, à des participants au programme de réemploi
- * de matériel informatique de l'entreprise MomoTech. Cette classe implémente l'interface OutilsConstantes.
+ * La classe AssignerMateriel permet d'assigner du matériel informatique, à des
+ * participants au programme de réemploi de matériel informatique de
+ * l'entreprise MomoTech. Cette classe implémente l'interface OutilsConstantes.
  */
 
 public class AssignerMateriel implements OutilsConstantes {
 
 	/**
-	 * Le constructeur AssignerMateriel() permet d'assigner du matériel informatique, à des participants au programme
-	 * de réemploi de matériel informatique de l'entreprise MomoTech. On assigne uniquement du matériel informatique
-	 * qui n'est pas déjà assigné à un participant.
+	 * Le constructeur AssignerMateriel() permet d'assigner du matériel
+	 * informatique, à des participants au programme de réemploi de matériel
+	 * informatique de l'entreprise MomoTech. On assigne uniquement du matériel
+	 * informatique qui n'est pas déjà assigné à un participant.
 	 * 
 	 * @param momoTech
 	 *            L'objet qui gère l'entreprise MomoTech.
@@ -26,12 +28,12 @@ public class AssignerMateriel implements OutilsConstantes {
 
 	public AssignerMateriel( MomoTech momoTech ) {
 		// Constantes locales.
-		final String MESS_MATERIEL_VIDE = "\nImpossible d'assigner du matériel informatique. " +
-				 				          "Il n'y a aucun matériel destiné au réemploi de disponible.";
-		final String MESS_TOUT_ASSIGNE = "\nImpossible d'assigner du matériel informatique. Tout le matériel " + 
-				 				         "destiné au réemploi est déjà assigné.";
-		final String MESS_PARTICIP_VIDE = "\nImpossible d'assigner du matériel informatique. Il n'y a aucun " + 
-                						  "participant dans l'entreprise MomoTech.";
+		final String MESS_MATERIEL_VIDE = "\nImpossible d'assigner du matériel informatique. "
+				+ "Il n'y a aucun matériel destiné au réemploi de disponible.";
+		final String MESS_TOUT_ASSIGNE = "\nImpossible d'assigner du matériel informatique. Tout le matériel "
+				+ "destiné au réemploi est déjà assigné.";
+		final String MESS_PARTICIP_VIDE = "\nImpossible d'assigner du matériel informatique. Il n'y a aucun "
+				+ "participant dans l'entreprise MomoTech.";
 		final String TITRE = "\nAssigner du matériel informatique.";
 		final String MESS_FIN_ASSIGNATION = "\nIl ne reste plus de matériel informatique à assigner.";
 		final String QUEST_AUTRE_ASSIGN = "\nVoulez-vous assigner un autre matériel informatique (O ou N) ? ";
@@ -55,7 +57,7 @@ public class AssignerMateriel implements OutilsConstantes {
 			Assignation assignation;
 			Participant participant;
 			int indMateriel, indAssignation, indParticipant;
-						
+
 			do {
 				System.out.println( TITRE );
 
@@ -63,8 +65,70 @@ public class AssignerMateriel implements OutilsConstantes {
 				 * TODO (À COMPLÉTER). Voir pages 15 et 16 de l'énoncé du TP5.
 				 */
 
+				materiel = MaterielInfo.creerMaterielInfo();
+				materiel.lireNoMateriel();
 
-				//------------------------------------------------------------
+				indMateriel = momoTech.getTabMateriel().chercher( materiel );
+
+				if ( indMateriel == -1 ) {
+
+					System.out.println(
+							"\nLe matériel informatique numéro " + materiel.getNoMateriel() + " n'existe pas." );
+
+				} else {
+
+					assignation = new Assignation( materiel.getNoMateriel() );
+					indAssignation = momoTech.getTabAssignations().chercher( assignation );
+
+					if ( indAssignation != -1 ) {
+
+						System.out.println( "\nLe matériel informatique numéro " + materiel.getNoMateriel()
+								+ " est déjà assigné à un participant." );
+
+					} else {
+
+						participant = new Participant();
+						participant.lireNoParticipant();
+
+						indParticipant = momoTech.getTabParticipants().chercher( participant );
+
+						if ( indParticipant == -1 ) {
+
+							System.out.println(
+									"\nLe participant numéro " + participant.getNoParticipant() + " n'existe pas." );
+
+						} else {
+
+							participant = (Participant) momoTech.getTabParticipants().obtenirObjet( indParticipant );
+
+							if ( participant.getTabAssignMat().estPlein() ) {
+
+								System.out.println( "\nL'assignation est impossible, car le participant numéro "
+										+ participant.getNoParticipant() + " a atteint le maximum d'assignations" );
+
+							} else {
+
+								assignation.setNoParticipant( participant.getNoParticipant() );
+								momoTech.getTabAssignations().ajouter( assignation );
+
+								materiel = (MaterielInfo) momoTech.getTabMateriel().obtenirObjet( indMateriel );
+
+								materiel.setAssigne( true );
+								participant.getTabAssignMat().ajouter( materiel );
+
+								System.out.println( "\nLe matériel informatique numéro " + materiel.getNoMateriel()
+										+ " est assigné au participant numéro " + participant.getNoParticipant()
+										+ "." );
+
+							}
+
+						}
+
+					}
+
+				}
+
+				// ------------------------------------------------------------
 				// Reste-t-il du matériel informatique qui n'est pas assigné ?
 
 				if ( momoTech.getTabAssignations().taille() == momoTech.getTabMateriel().taille() ) {
