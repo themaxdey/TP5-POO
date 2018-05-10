5e411d60ba03319eac03091ce7a1486f0462eac8
themaxdey <maximedery@gmail.com>
2018-05-09 20:14:55 -0400

    Visual bugs

--- TP5-POO/src/TerminerAssignation.java
+++ TP5-POO/src/TerminerAssignation.java
@@ -18,18 +18,18 @@ import outilsjava.*;
 public class TerminerAssignation implements OutilsConstantes {
 
 	/**
-	 * Le constructeur TerminerAssignation() permet de terminer une assignation
-	 * de matériel informatique, à des participants au programme de réemploi de
-	 * matériel informatique de l'entreprise MomoTech. On termine une
-	 * assignation uniquement sur du matériel informatique qui a préalablement
-	 * été assigné à un participant. Le matériel assigné est lui aussi supprimé,
-	 * puisqu'il est maintenant prêt à être réutilisé.
+	 * Le constructeur TerminerAssignation() permet de terminer une assignation de
+	 * matériel informatique, à des participants au programme de réemploi de
+	 * matériel informatique de l'entreprise MomoTech. On termine une assignation
+	 * uniquement sur du matériel informatique qui a préalablement été assigné à un
+	 * participant. Le matériel assigné est lui aussi supprimé, puisqu'il est
+	 * maintenant prêt à être réutilisé.
 	 * 
 	 * @param momoTech
 	 *            L'objet qui gère l'entreprise MomoTech.
 	 */
 
-	public TerminerAssignation( MomoTech momoTech ) {
+	public TerminerAssignation(MomoTech momoTech) {
 		// Constantes locales.
 		final String MESS_MATERIEL_VIDE = "\nImpossible de terminer une assignation. "
 				+ "Il n'y a aucun matériel destiné au réemploi de disponible.";
@@ -42,13 +42,13 @@ public class TerminerAssignation implements OutilsConstantes {
 
 		char rep = NON;
 
-		if ( momoTech.getTabMateriel().estVide() ) {
+		if (momoTech.getTabMateriel().estVide()) {
 
-			System.out.println( MESS_MATERIEL_VIDE );
+			System.out.println(MESS_MATERIEL_VIDE);
 
-		} else if ( momoTech.getTabAssignations().estVide() ) {
+		} else if (momoTech.getTabAssignations().estVide()) {
 
-			System.out.println( MESS_AUCUNE_ASSIGNATION );
+			System.out.println(MESS_AUCUNE_ASSIGNATION);
 
 		} else {
 			MaterielInfo materiel;
@@ -58,44 +58,45 @@ public class TerminerAssignation implements OutilsConstantes {
 
 			do {
 
-				System.out.println( TITRE );
+				System.out.println(TITRE);
 
 				materiel = MaterielInfo.creerMaterielInfo();
 
 				materiel.lireNoMateriel();
 
-				indMateriel = momoTech.getTabMateriel().chercher( materiel );
+				indMateriel = momoTech.getTabMateriel().chercher(materiel);
 
-				if ( indMateriel == -1 ) {
-					System.out.println( "Le matériel informatique numéro " + indMateriel + " n'existe pas." );
+				if (indMateriel == -1) {
+					System.out.println(
+							"\nLe matériel informatique numéro " + materiel.getNoMateriel() + " n'existe pas.");
 				} else {
-					assignation = new Assignation( materiel.getNoMateriel() );
+					assignation = new Assignation(materiel.getNoMateriel());
 
-					indAssignation = momoTech.getTabAssignations().chercher( assignation );
+					indAssignation = momoTech.getTabAssignations().chercher(assignation);
 
-					if ( indAssignation == -1 ) {
-						System.out.println( "\nLe matériel informatique numéro " + assignation.getNoMateriel()
-								+ " n'est pas assigné à un participant" );
+					if (indAssignation == -1) {
+						System.out.println("\nLe matériel informatique numéro " + materiel.getNoMateriel()
+								+ " n'est pas assigné à un participant");
 					} else {
-						assignation = (Assignation) momoTech.getTabAssignations().obtenirObjet( indAssignation );
+						assignation = (Assignation) momoTech.getTabAssignations().obtenirObjet(indAssignation);
 
-						momoTech.getTabAssignations().supprimer( assignation );
+						momoTech.getTabAssignations().supprimer(assignation);
 
 						participant = new Participant();
 
-						participant.setNoParticipant( assignation.getNoParticipant() );
+						participant.setNoParticipant(assignation.getNoParticipant());
 
-						indParticipant = momoTech.getTabParticipants().chercher( participant );
+						indParticipant = momoTech.getTabParticipants().chercher(participant);
 
-						participant = (Participant) momoTech.getTabParticipants().obtenirObjet( indParticipant );
+						participant = (Participant) momoTech.getTabParticipants().obtenirObjet(indParticipant);
 
-						participant.getTabAssignMat().supprimer( materiel );
+						participant.getTabAssignMat().supprimer(materiel);
 
-						momoTech.getTabMateriel().supprimer( indMateriel );
+						momoTech.getTabMateriel().supprimer(indMateriel);
 
 						System.out.println(
 								"\nLe matériel numéro " + indMateriel + " qui était assigné au participant numéro "
-										+ indParticipant + " est prêt à être réutilisé." );
+										+ indParticipant + " est prêt à être réutilisé.");
 					}
 
 				}
@@ -103,14 +104,14 @@ public class TerminerAssignation implements OutilsConstantes {
 				// -------------------------------------------------------
 				// Reste-t-il des assignations de matériel informatique ?
 
-				if ( momoTech.getTabAssignations().estVide() ) {
+				if (momoTech.getTabAssignations().estVide()) {
 
-					System.out.println( MESS_FIN_TERMINER );
+					System.out.println(MESS_FIN_TERMINER);
 					rep = NON;
 				} else {
-					rep = OutilsLecture.lireOuiNon( QUEST_TERMINER_AUTRE_ASSIGN );
+					rep = OutilsLecture.lireOuiNon(QUEST_TERMINER_AUTRE_ASSIGN);
 				}
-			} while ( rep == OUI );
+			} while (rep == OUI);
 		}
 	}
 }
