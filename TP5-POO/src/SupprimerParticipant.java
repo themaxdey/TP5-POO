cd0515691216ea3361ba37a4f8e7cd421ee3c68a
themaxdey <maximedery@gmail.com>
2018-05-09 20:10:17 -0400

    Jean-Seb pis sa marde de regler

--- TP5-POO/src/SupprimerParticipant.java
+++ TP5-POO/src/SupprimerParticipant.java
@@ -17,16 +17,16 @@ import outilsjava.*;
 public class SupprimerParticipant implements OutilsConstantes {
 
 	/**
-	 * Le constructeur SupprimerParticipant() permet de gérer les suppressions
-	 * de participants au programme de réemploi de matériel informatique de
-	 * MomoTech. On ne peut pas supprimer un participant à qui du matériel
-	 * informatique a été assigné.
+	 * Le constructeur SupprimerParticipant() permet de gérer les suppressions de
+	 * participants au programme de réemploi de matériel informatique de MomoTech.
+	 * On ne peut pas supprimer un participant à qui du matériel informatique a été
+	 * assigné.
 	 * 
 	 * @param momoTech
 	 *            L'objet qui gère l'entreprise MomoTech.
 	 */
 
-	public SupprimerParticipant( MomoTech momoTech ) {
+	public SupprimerParticipant(MomoTech momoTech) {
 		// Constantes locales.
 		final String MESS_VIDE = "\nImpossible de supprimer un participant. Il n'y a aucun "
 				+ "participant dans l'entreprise MomoTech.";
@@ -38,49 +38,50 @@ public class SupprimerParticipant implements OutilsConstantes {
 
 		char rep = NON;
 
-		if ( momoTech.getTabParticipants().estVide() ) {
-			System.out.println( MESS_VIDE );
+		if (momoTech.getTabParticipants().estVide()) {
+			System.out.println(MESS_VIDE);
 		} else {
 			Participant participant;
 			int indParticipant;
 
 			do {
-				System.out.println( TITRE );
+				System.out.println(TITRE);
 
 				participant = new Participant();
 
 				participant.lireNoParticipant();
 
-				indParticipant = momoTech.getTabParticipants().chercher( participant );
+				indParticipant = momoTech.getTabParticipants().chercher(participant);
 
-				if ( indParticipant == -1 ) {
-					System.out.println( MESS_VIDE );
+				if (indParticipant == -1) {
+					System.out.println(MESS_VIDE);
 				} else {
 					participant.afficher();
 
-					char confirmation = OutilsLecture.lireOuiNon( QUEST_CONF_SUPP );
+					char confirmation = OutilsLecture.lireOuiNon(QUEST_CONF_SUPP);
 
-					if ( confirmation == OUI ) {
-						momoTech.getTabParticipants().supprimer( indParticipant );
+					if (confirmation == OUI) {
+						momoTech.getTabParticipants().supprimer(indParticipant);
 
-						System.out.println( "Le participant numéro " + indParticipant + " a été supprimé." );
-					} else {
 						System.out.println(
-								"La suppression du participant numéro " + indParticipant + " a été annulée." );
+								"\nLe participant numéro " + participant.getNoParticipant() + " a été supprimé.");
+					} else {
+						System.out.println("\nLa suppression du participant numéro " + participant.getNoParticipant()
+								+ " a été annulée.");
 					}
 				}
 
 				// ---------------------------------------------------------
 				// Reste-t-il des participants dans l'entreprise MomoTech ?
 
-				if ( momoTech.getTabParticipants().estVide() ) {
+				if (momoTech.getTabParticipants().estVide()) {
 
-					System.out.println( MESS_DEVENU_VIDE );
+					System.out.println(MESS_DEVENU_VIDE);
 					rep = NON;
 				} else {
-					rep = OutilsLecture.lireOuiNon( QUEST_AUTRE_PARTICIPANT );
+					rep = OutilsLecture.lireOuiNon(QUEST_AUTRE_PARTICIPANT);
 				}
-			} while ( rep == OUI );
+			} while (rep == OUI);
 		}
 	}
 }
